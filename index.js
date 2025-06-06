4964037e86a8d5d30429c602c2cb8d4e82cabee4
Beatrice Wacera KIBUCHA <beatricekibucha@gmail.com>
2025-06-07 00:17:33 +0300

    add changes

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+
+const currentUser = 'Wacera';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
